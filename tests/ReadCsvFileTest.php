334c843717bee7feeaca625b5564cdc4bb9769b0
smaine <smaine.milianni@gmail.com>
2017-12-27 08:46:26 +0100

    tdd on readcsvfile

---
+++ tests/ReadCsvFileTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eniams
+ * Date: 27/12/17
+ * Time: 08:41
+ */
+
+namespace Tests\ReadCsvFileTest;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ReadCsvFileTest extends WebTestCase
+{
+    public function readCsvFileWhenExists()
+    {
+
+    }
+
+    public function readCsvFileWhenNotExists()
+    {
+
+    }
+
+    public function readCsvFileWhenEmpty()
+    {
+
+    }
+
+    public function readCsvFileWhenWrongFormat()
+    {
+
+    }
+}
